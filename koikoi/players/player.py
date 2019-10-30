e4fc88732a2503617923245d22359b3458753fb8
moritagit <ryousuke654@gmail.com>
2019-10-31 02:54:57 +0900

    make an abstract class for players

---
+++ koikoi/players/player.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+
+from typing import List
+
+from koikoi.card import Card, ShareCards
+from koikoi.field import Field
+
+
+class Player(object):
+    """
+    An abstract class for players.
+
+    Parameters
+    ----------
+    cards : ``List[Card]``
+    name : ``str``, optional (default = 'Player')
+    """
+    def __init__(
+        self,
+        cards: List[Card],
+        name: str = 'Player',
+    ) -> None:
+
+        self.hand = cards
+        self.name = name
+        self.share = ShareCards()
+
+    def select_from_hand(self, field, other) -> Card:
+        """
+        Selects card from hand in this player's turn.
+
+        Paramteres
+        ----------
+        field : ``Field``
+        other : ``Player``
+
+        Return
+        ------
+        card : ``Card``
+        """
+        raise NotImplementedError()
+
+    def select_from_field(self, field, other) -> Card:
+        """
+        Selects card from field when two cards are the same month
+        as the player put on the field.
+
+        Paramteres
+        ----------
+        field : ``Field``
+        other : ``Player``
+
+        Return
+        ------
+        card : ``Card``
+        """
+        raise NotImplementedError()
+
+    def koikoi(self, field, other) -> bool:
+        """
+        Determines whether to koikoi or not.
+
+        Paramteres
+        ----------
+        field : ``Field``
+        other : ``Player``
+
+        Return
+        ------
+        is_koikoi : ``bool``
+        """
+        raise NotImplementedError()
