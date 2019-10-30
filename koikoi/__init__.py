60ac403f8b90fd00c8662e8fd9034256579e90ad
moritagit <ryousuke654@gmail.com>
2019-10-31 03:33:05 +0900

    write __init__.py

---
+++ koikoi/__init__.py
@@ -0,0 +1,14 @@
+from koikoi.card import Card, Deck
+from koikoi.field import Field
+from koikoi.players import Player
+from koikoi.players import Human, RandomCPU
+
+
+__all__ = [
+    'Card',
+    'Deck',
+    'Field',
+    'Player',
+    'Human',
+    'RandomCPU'
+]
---
+++ koikoi/players/__init__.py
@@ -0,0 +1,10 @@
+from koikoi.players.player import Player
+from koikoi.players.human import Human
+from koikoi.players.random import RandomCPU
+
+
+__all__ = [
+    'Player',
+    'Human',
+    'RandomCPU'
+]
