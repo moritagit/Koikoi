901b321798d246dfe8a36d8e0701553bd3d27acb
moritagit <ryousuke654@gmail.com>
2019-10-31 06:40:26 +0900

    add some methods

--- koikoi/formatters/formatter.py
+++ koikoi/formatters/formatter.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 
+from typing import List
+
 from koikoi.card import Card
 from koikoi.field import Field
 from koikoi.players import Player
@@ -11,16 +13,25 @@ class Formatter(object):
     An abstract class for formatters.
     Formatter outputs cards in field and player hands.
     """
-    def format_field(self, field: Field):
+    def format_turn(self, player: Player):
         pass
 
     def format_hand(self, player: Player):
+        pass
+
+    def format_share(self, player: Player):
+        pass
+
+    def format_field(self, field: Field):
         pass
 
     def format_card_selection(self, card: Card):
         pass
 
     def format_draw(self, card: Card):
+        pass
+
+    def format_yaku(self, updated_yaku: List[str]):
         pass
 
     def format_end_message(self, message: str):
