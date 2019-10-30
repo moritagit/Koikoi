4d538da610dfd94757dd99cf4d66916e4c08f14a
moritagit <ryousuke654@gmail.com>
2019-10-31 02:35:41 +0900

    delete hand.py, rename Hand to ShareCards, and move it to cards.py

--- koikoi/card.py
+++ koikoi/card.py
@@ -228,3 +228,27 @@ class Deck(object):
 
     def pop(self) -> Card:
         return self.cards.pop(0)
+
+
+class ShareCards(object):
+    """Represents cards player got."""
+    def __init__(self) -> None:
+        self.data = {
+            'light': [],
+            'seed': [],
+            'strip': [],
+            'kasu': [],
+        }
+
+    def __len__(self) -> int:
+        return sum(self.tolist())
+
+    def tolist(self) -> List[Card]:
+        return [len(lst) for lst in self.data.values()]
+
+    def append(self, card: Card) -> None:
+        self.data[card.role_class].append(card)
+
+    def extend(self, cards: List[Card]) -> None:
+        for card in cards:
+            self.append(card)
