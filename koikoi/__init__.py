185f3c4c643ce38da9828609c7721ad3736b78ed
moritagit <ryousuke654@gmail.com>
2019-10-31 06:51:10 +0900

    add modiles to __init__.py

--- koikoi/__init__.py
+++ koikoi/__init__.py
@@ -1,5 +1,7 @@
 from koikoi.card import Card, Deck
 from koikoi.field import Field
+from point_calculator import PointCalculator
+from koikoi.formatters import Formatter, Console
 from koikoi.players import Player
 from koikoi.players import Human, RandomCPU
 
@@ -8,6 +10,9 @@ __all__ = [
     'Card',
     'Deck',
     'Field',
+    'PointCalculator',
+    'Formatter',
+    'Console',
     'Player',
     'Human',
     'RandomCPU'
