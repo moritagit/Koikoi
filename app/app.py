39bf5ef7f3f92d42ad66da502fb438f8482fd742
moritagit <ryousuke654@gmail.com>
2019-11-01 07:17:43 +0900

    write app up

--- app/app.py
+++ app/app.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 import koikoi
 
 
 app = Flask(__name__)
 
-gamemaster = koikoi.GameMaster()
+gamemaster = koikoi.GameMaster(
+    player1=koikoi.players.HumanFlask(),
+    player2=koikoi.players.RandomCPU(),
+    formatter=koikoi.Console(),
+)
 
 
 @app.route('/')
@@ -16,6 +20,7 @@ def main():
     gamemaster.build()
     return render_template(
         'index.html',
+        message='はじめます',
         field=gamemaster.field,
         player=gamemaster.player1,
         other=gamemaster.player2,
@@ -24,11 +29,32 @@ def main():
 
 @app.route('/select', methods=['POST'])
 def select():
-    selected_card = koikoi.Card.from_string(request.form['selected'])
-    gamemaster.build()
+    message = 'あなたの番です'
+
+    # human turn
+    was_human_win = gamemaster.process_one_turn(
+        gamemaster.player1, gamemaster.player2,
+    )
+
+    if was_human_win:
+        point = sum([val for val in gamemaster.player1.point_data.values()])
+        message = f'{point}文であなたの勝ちです'
+    else:
+        # CP turn
+        was_cp_win = gamemaster.process_one_turn(
+            gamemaster.player2, gamemaster.player1,
+        )
+        if was_cp_win:
+            point = sum([val for val in gamemaster.player2.point_data.values()])
+            message = f'{point}文でCPの勝ちです'
+
+    is_draw = (not (gamemaster.player1.hand or gamemaster.player2.hand))
+    if is_draw:
+        message = '流れです'
 
     return render_template(
         'index.html',
+        message=message,
         field=gamemaster.field,
         player=gamemaster.player1,
         other=gamemaster.player2,
