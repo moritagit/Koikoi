b767e3e94fea98064eb122ac6f946607b78e403d
moritagit <ryousuke654@gmail.com>
2019-11-01 06:53:01 +0900

    update __init__.py

--- koikoi/players/__init__.py
+++ koikoi/players/__init__.py
@@ -1,10 +1,12 @@
 from koikoi.players.player import Player
 from koikoi.players.human import Human
+from koikoi.players.human_flask import HumanFlask
 from koikoi.players.random import RandomCPU
 
 
 __all__ = [
     'Player',
     'Human',
+    'HumanFlask',
     'RandomCPU'
 ]
