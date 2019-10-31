eba9aae74637b5a3a90fb01d3cc34997900c7899
moritagit <ryousuke654@gmail.com>
2019-11-01 05:56:47 +0900

    modify around sake yaku

--- koikoi/point_calculator.py
+++ koikoi/point_calculator.py
@@ -52,7 +52,7 @@ class PointCalculator(object):
         if n2c['菊の盃'] in share.data['seed']:
             if n2c['桜の幕'] in share.data['light']:
                 point_data['花見酒'] = y2p['花見酒']
-            elif n2c['芒の月'] in share.data['light']:
+            if n2c['芒の月'] in share.data['light']:
                 point_data['月見酒'] = y2p['月見酒']
 
         # tane
