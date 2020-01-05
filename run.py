56e5c65b983217e598ed7bb06bdc63a176a86ff8
moritagit <ryousuke654@gmail.com>
2020-01-05 17:14:27 +0900

    introduce click and enable to run in console

--- run.py
+++ run.py
@@ -1,8 +1,37 @@
 # -*- coding: utf-8 -*-
 
 
-from app.app import app
+import click
+
+
+@click.command()
+@click.option('--mode', '-m', default='console')
+def cmd(mode: str):
+    if mode.lower() == 'console':
+        run_console()
+    elif mode.lower() == 'app':
+        run_app()
+    else:
+        raise ValueError(f'Unknown mode {mode}')
+
+
+def run_console():
+    import koikoi
+    gm = koikoi.GameMaster(
+        formatter=koikoi.Console(),
+    )
+    gm.build()
+    gm.run()
+
+
+def run_app():
+    from app.app import app
+    app.run()
+
+
+def main():
+    cmd()
 
 
 if __name__ == '__main__':
-    app.run()
+    main()
